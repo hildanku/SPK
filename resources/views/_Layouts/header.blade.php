d99a788a18e07af5cf397e337cb8e4cfbb8dcfba
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-06-27 01:11:12 +0700

    chore(header.blade): fix csrf issue

--- resources/views/_Layouts/header.blade.php
+++ resources/views/_Layouts/header.blade.php
@@ -2,8 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Voler Admin Dashboard</title>
+    <title>Dashboard - SPK</title>
     
     <link rel="stylesheet" href="/front/dist/assets/css/bootstrap.css">
     
