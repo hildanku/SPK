8576ea3ae39a5f5fc69b03d02d3e31d528612450
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-06-27 01:18:27 +0700

    chore: change routing

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\tes;
 use App\Http\Controllers\CriteriaController;
+use App\Http\Controllers\FoodController;
+use App\Http\Controllers\CalculateController;
+use App\Http\Controllers\SAWController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,13 +19,25 @@ use App\Http\Controllers\CriteriaController;
 */
 // PR EDIT ROUTES ASSETS -- clear
 // BREAK ISYA
-Route::get('/', function () {
-    return view('welcome');
-});
 
- 
-Route::get('/tes', [tes::class, 'index']);
 
-Route::get('/criteria', [CriteriaController::class, 'index']);
+Route::get('/criterias', [CriteriaController::class, 'index']);
+Route::get('/criteria/create', [CriteriaController::class, 'create']);
+Route::post('/criteria/store', [CriteriaController::class, 'store']);
 Route::get('/criteria/edit/{id}', [CriteriaController::class, 'edit']);
 Route::post('/criteria/update/{id}', [CriteriaController::class, 'update']);
+
+Route::get('/foods', [FoodController::class, 'index']);
+Route::get('/food/edit/{id}', [FoodController::class, 'edit']);
+Route::post('/food/update/{id}', [FoodController::class, 'update']);
+Route::get('/food/create', [FoodController::class, 'create']);
+Route::post('/food/store', [FoodController::class, 'store']);
+Route::post('/food/delete/{id}', [FoodController::class, 'destroy']);
+Route::get('/food/calculateSAW', [FoodController::class, 'calculateSAW']);
+
+
+Route::get('/calculate-saw-fake', [FoodController::class, 'calculateSAWWithFakeData']);
+
+
+Route::get('/calculate-saw', [SAWController::class, 'calculateSAW'])->name('calculate-saw');
+Route::get('/calc', [SAWController::class, 'index'])->name('calculate-saw');
