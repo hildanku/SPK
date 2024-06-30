682b409e68611d4aa47f6261549c05452fabbb56
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-06-30 21:47:16 +0700

    feat: added dashboard page

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('_Layouts.master')
+
+@section('content')
+<section class="section">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Dashboard</h5>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
