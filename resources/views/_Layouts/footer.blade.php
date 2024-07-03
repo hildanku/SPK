e3ab50d03b8373ea4db5175b5e9ef07a5e4c4837
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-07-03 23:23:48 +0700

    chore(views/footer): change footer content

--- resources/views/_Layouts/footer.blade.php
+++ resources/views/_Layouts/footer.blade.php
@@ -3,10 +3,10 @@
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
                     <div class="float-start">
-                        <p>2022 &copy; Voler</p>
+                        <p>2022 &copy; Template by Voler (Ahmad Saugi)</p>
                     </div>
                     <div class="float-end">
-                        <p>Crafted with <span class='text-danger'><i data-feather="heart"></i></span> by <a href="https://saugi.me">Saugi</a></p>
+                        <p>Develop with <span class='text-danger'><i data-feather="heart"></i></span> by <a href="https://saugi.me">Kelompok3</a></p>
                     </div>
                 </div>
             </footer>
